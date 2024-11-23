7395df4399619a6e6dd9015983b3590ce964c8c0
Haocheng Yang <alexyanghaocheng@gmail.com>
2024-11-23 11:29:36 +0000

    add join & host, two way data binding

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,9 +3,11 @@
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
   import Button from './lib/Button.svelte';
-    import QuizCard from './lib/QuizCard.svelte';
+  import QuizCard from './lib/QuizCard.svelte';
+  import Join from './lib/Join.svelte';
 
-  let quizzes: {_id: string, name: string}[] = [];
+  let pin = $state("");
+  let quizzes: {_id: string, name: string}[] = $state([]);
 
   async function getQuizzes(){
     let response = await fetch("http://localhost:3000/api/quizzes");
@@ -19,11 +21,23 @@
     console.log(json);
   }
   
-  function webcocket_connect(){
+  function join(){
     let websocket = new WebSocket("ws://localhost:3000/ws");
     websocket.onopen = () => {
-      console.log("opened websocket connection");
-      websocket.send("echo!");
+      console.log("joined!");
+      websocket.send(`join:${pin}`);
+    };
+
+    websocket.onmessage = (event) => {
+      console.log(event.data);
+    }
+  }
+
+  function host(quiz){
+    let websocket = new WebSocket("ws://localhost:3000/ws");
+    websocket.onopen = () => {
+      console.log("hosting!");
+      websocket.send(`host:${quiz.name}`);
     };
 
     websocket.onmessage = (event) => {
@@ -33,51 +47,10 @@
 </script>
 
 <Button onclick={getQuizzes}>getQuizzes</Button>
-<Button onclick={webcocket_connect}>Websocket Connect</Button>
+<Join bind:joinPin={pin} handleJoin={join}></Join>
 
 {#each quizzes as quiz}
-  <QuizCard onhost={webcocket_connect} {quiz}/>
+  <QuizCard onhost={() => host(quiz)} {quiz}/>
 {/each}
 
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ frontend/src/lib/Join.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Button from "./Button.svelte";
+
+    let { joinPin = $bindable(""), handleJoin } = $props();
+  
+  </script>
+  
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="w-full max-w-sm p-6 bg-white border border-gray-300 rounded-lg shadow-md">
+      <input
+        type="text"
+        placeholder="Game PIN"
+        bind:value={joinPin}
+        class="w-full px-4 py-2 mb-4 text-lg border border-gray-300 rounded focus:ring-2 focus:ring-purple-500 focus:outline-none"
+      />
+      <Button onclick={handleJoin}>Enter</Button>
+    </div>
+  </div>
